a30e3f6bac2166d20aa0f85f430c056bf3534292
Dhavisco <olaniyidavid2002@gmail.com>
2024-12-30 14:03:21 +0100

    framer motion and typescript

--- src/components/QuotesSection.tsx
+++ src/components/QuotesSection.tsx
@@ -1,4 +1,5 @@
 import { useQuotes } from '../hooks/useQuotes';
+import { motion } from 'framer-motion';
 import './../styles/quotes.scss'
 // import { FaArrowsRotate } from "react-icons/fa6";
 import refreshIcon from './../assets/desktop/icon-refresh.svg'
@@ -25,17 +26,33 @@ const QuotesSection = ({isExpanded}:QuotesSectionProps) => {
   return (
 <>
    {!isExpanded && quoteData ?
-    (<div className="quote-container">
-      <div className='quote-content'>
+    (<motion.div className="quote-container"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 1 }}
+    >
+      <motion.div className='quote-content'
+      key={quoteData.quote}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      >
         <div>{quoteData.quote}</div>
         <div className='author'>{quoteData.author}</div>
-      </div>
+      </motion.div>
 
-      <button onClick={refreshQuotes}>
+      <motion.button onClick={refreshQuotes}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      // whileHover={{ rotate: 360, scale: 1.2 }}
+      transition={{ duration: 0.5 }}
+      >
         {/* <FaArrowsRotate/> */}
         <img src={refreshIcon} alt="" />
-      </button>
-    </div>
+      </motion.button>
+    </motion.div>
     ) : (
       <p>{isExpanded ? "" : "Error Loading Quotes"}</p>
     )
--- src/components/TimeSection.tsx
+++ src/components/TimeSection.tsx
@@ -3,6 +3,7 @@ import { useLocation } from "../hooks/useLocation";
 import { useTime } from "../hooks/useTime";
 import { useUpdateBackground } from "../hooks/useUpdateBackground";
 import './../styles/time.scss'
+import { motion } from "framer-motion";
 
 import SunIcon from './../assets/desktop/icon-sun.svg';
 import MoonIcon from './../assets/desktop/icon-moon.svg';
@@ -11,6 +12,7 @@ import ArrowUpIcon from './../assets/desktop/icon-arrow-up.svg';
 // import { TbChristmasTreeFilled } from "react-icons/tb";
 import { MdCelebration } from "react-icons/md";
 import Preloader from "./Preloader";
+import Typewriter from "typewriter-effect";
 
 
 interface TimeSectionProps {
@@ -27,7 +29,7 @@ export const TimeSection = ({toggleExpand, isExpanded}:TimeSectionProps) => {
     greeting: "Good Morning",
     icon: SunIcon
   });
-//   const [isExpanded, setIsExpanded] = useState(false)
+
   const timeOfDay = useUpdateBackground();
 
   const specialSeasonMessage = 'HAPPY NEW YEAR IN ADVANCE!'
@@ -69,47 +71,47 @@ export const TimeSection = ({toggleExpand, isExpanded}:TimeSectionProps) => {
     return () => clearInterval(intervalId); // Cleanup
   }, []);
 
-  //Toggle expanded view
- 
+
+
 
   if (timeLoading || locationLoading) {
     return <Preloader />;
   }
 
   return (
-//     <div className="time-section">
-//      <div className={`time-container`}>
-//       <div className={`time-content ${isExpanded ? "expanded" : ""}`}>
-//         <div className="greeting">
-//             <img src={timeInfo.icon} alt="Icon"/>
-//              <div>{timeInfo.greeting}<span className="current">, IT'S CURRENTLY</span></div>
-//         </div>
-//         <div className="time">
-//           {currentTime} <span>{timeData?.abbreviation}</span>
-//         </div>
-//         <div className="location">
-//           IN {locationData?.city}, {locationData?.country_code}
-//         </div>
-//       </div>
-//        <div className="time-button" onClick={toggleExpand}>
-//         <div>{isExpanded ? "LESS" : "MORE"}</div>
-//         <div className="time-arrow">
-//           <img src={isExpanded ? ArrowUpIcon : ArrowDownIcon} alt="Toggle Icon" />
-//         </div>
-//       </div>
-//     </div>
 
-//  {/* Expanded Content */}
-//       { isExpanded && 
-//       <div className="expanded-content">
-//         <div className="details">
-//           <p>Current Timezone: <span>{timeData?.timezone}</span></p>
-//           <p>Day of the Year: <span>{timeData?.day_of_year}</span></p>
-//           <p>Day of the Week: <span>{timeData?.day_of_week}</span></p>
-//           <p>Week Number: <span>{timeData?.week_number}</span></p>
-//         </div>
-//       </div>}
-//     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
    <div className="time-container">
@@ -118,7 +120,17 @@ export const TimeSection = ({toggleExpand, isExpanded}:TimeSectionProps) => {
     
          <div className="special-season">
         < MdCelebration className="tree"/>
-        {specialSeasonMessage}</div>
+        <Typewriter
+        options={{ loop: true }}
+        onInit={(typewriter) => {
+        typewriter
+          .typeString(specialSeasonMessage)
+          .pauseFor(2000) // Pause before deleting
+          .deleteAll()
+          .start();
+      }}
+  />
+        </div>
       <div className="greeting">
         <img src={timeInfo.icon} alt="Icon" />
         <div>
@@ -126,9 +138,41 @@ export const TimeSection = ({toggleExpand, isExpanded}:TimeSectionProps) => {
           <span className="current">, IT'S CURRENTLY</span>
         </div>
       </div>
-      <div className="time">
+
+      {/* Current time */}
+      <motion.div 
+
+      key={currentTime}
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.8 }}
+      className="time">
         {currentTime} <span>{timeData?.abbreviation}</span>
-      </div>
+        {/* Clock indicator */}
+  <motion.div
+    className="clock-indicator"
+    initial={{ scale: 1 }}
+    animate={{
+      scale: [1, 1.2, 1],
+      rotate: [0, 360],
+    }}
+    transition={{
+      duration: 2,
+      repeat: Infinity,
+      ease: "easeInOut",
+    }}
+    style={{
+      marginLeft: "10px",
+      width: "10px",
+      height: "10px",
+      borderRadius: "50%",
+
+      backgroundColor: "green",
+    }}
+  />
+      </motion.div>
+
       <div className="location">
         IN {locationData?.city}, {locationData?.country_code}
       </div>
@@ -141,7 +185,11 @@ export const TimeSection = ({toggleExpand, isExpanded}:TimeSectionProps) => {
     </div>
   </div>
   {isExpanded && (
-    <div className={`expanded-content ${timeOfDay === "day" ? "day" : "night"}`}>
+    <motion.div className={`expanded-content ${timeOfDay === "day" ? "day" : "night"}`}
+    initial={{ height: 0, opacity: 0 }}
+        animate={isExpanded ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }}
+        transition={{ type: "spring", stiffness: 200, damping: 20 }}
+    >
       <div className="details">
         <div className="time-year">
             <div className="timezone details-wrap">
@@ -166,10 +214,11 @@ export const TimeSection = ({toggleExpand, isExpanded}:TimeSectionProps) => {
         </div>
         
       </div>
-    </div>
+    </motion.div>
   )}
 </div>
 
 
   );
+
 };
